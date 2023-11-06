b13c153def0b84eff2905d494fac65898582dc3c
Abhinandan Udupa <67597710+abhinandanudupa@users.noreply.github.com>
2023-11-06 14:54:08 +0530

    [bugfix] Correct None comparison + other features
    - Save details of mentors and mentees who are not allocated any
    - Other muscellaneous changes

--- allocator.py
+++ allocator.py
@@ -3,6 +3,7 @@
 
 from csv import DictReader, DictWriter
 from datetime import datetime
+
 # from pprint import pprint
 
 
@@ -26,10 +27,13 @@ DPT_MPPNG = {
     "Medical Electronics": "ML",
 }
 SAVE_FILE = "allocations.csv"
+SAVE_NOT_MENTEE = "not_allocated_mentees.csv"
+SAVE_NOT_MENTOR = "not_allocated_mentors.csv"
 
 
 class Mentee:
     """A class representing a mentee and with methods to check prefference and rank mentors"""
+
     def __init__(self, name, email, year, country, reg_time, ctype, department, prefs):
         self.year = year
         self.reg_time = reg_time
@@ -53,11 +57,11 @@ class Mentee:
             return float("inf")
 
     def __lt__(self, other):
-        if self.year < other.year:
-            return True
-        if self.reg_time < other.reg_time:
-            return True
-        return False
+        return (self.year, self.reg_time, self.name) < (
+            other.year,
+            other.reg_time,
+            self.name,
+        )
 
     def __eq__(self, other):
         return self.year == other.year and self.reg_time == other.reg_time
@@ -75,10 +79,11 @@ class Mentee:
 
 class Mentor:
     """A class to represent a mentor with methods to macth with a mentee."""
+
     def __init__(self, name, email, country, ctype, year, department, capacity):
-        self.capacity = capacity
+        self.capacity = 2  # capacity
+        self.total_capacity = 2  # capacity
         self.country = country
-        self.allocated = []
         self.name = name
         self.email = email
         self.year = year
@@ -98,28 +103,24 @@ class Mentor:
 {self.email}
 {self.ctype}
 {self.capacity}
-{self.country}
-{self.allocated}"""
+{self.country}"""
 
 
 def allocate_mentors(mentee_list, mentor_list):
     """Allocates mentors to mentees on the basis of their preference and need"""
     allocated = []
     for mentee in mentee_list:
+        print("Mentee", mentee.name)
         best_mentor = (None, float("inf"))
         for mentor in mentor_list:
+            rank = mentee.get_preference_rank(mentor)
             if mentee.check_preference(mentor) and mentor.has_capacity():
-                rank = mentee.get_preference_rank(mentor)
-                if rank < best_mentor[1]:
-                    best_mentor = (mentor, rank)
-
-            elif mentor.check_match(mentee) and mentor.has_capacity():
-                rank = mentee.get_preference_rank(mentor)
                 if rank < best_mentor[1]:
                     best_mentor = (mentor, rank)
 
         bm, br = best_mentor
-        if bm is not None:
+        print("Mentor", bm)
+        if not bm is None:
             allocated.append((mentee, bm, br))
             bm.capacity -= 1
             # print(bm)
@@ -153,7 +154,7 @@ def parse_mentees(csv_file):
             # pprint(row)
             name = row["Full Name"]
             email = row["Email Address"]
-            ctype = "t" if row["Type of course"] == "Technical - MS, PhD etc" else "m"
+            ctype = "t"  # "t" if row["Type of course"] == "Technical - MS, PhD etc" else "m"
             prefs = []
             prefs.append(row["Mentor 1"].split(" - ")[1])
             prefs.append(row["Mentor 2"].split(" - ")[1])
@@ -162,12 +163,6 @@ def parse_mentees(csv_file):
             department = DPT_MPPNG[row["Department"]]
             reg_time = row["Timestamp"]
             reg_time = datetime.strptime(reg_time, "%m/%d/%Y %H:%M:%S")
-            reg_time = (
-                reg_time.day * 3600 * 24
-                + reg_time.hour * 3600
-                + reg_time.minute * 60
-                + reg_time.second
-            )
             country = parse_country(
                 row["One country you are interested in for higher education"]
             )
@@ -190,7 +185,7 @@ def parse_mentors(csv_file):
             email = row["Email Address"]
             year = int(row["Year of Passout (YYYY)"])
             country = parse_country(row["Country in which University is located"])
-            ctype = row["ctype"]
+            ctype = "t"  # row["ctype"]
             department = DPT_MPPNG[row["Department in BMSCE"]]
             capacity = int(row["No of Mentee"])
             mentor = Mentor(name, email, country, ctype, year, department, capacity)
@@ -202,15 +197,18 @@ def parse_mentors(csv_file):
 mentors = parse_mentors("./mentors.csv")
 N_MENTORS = len(mentors)
 
+# print("------------------------------------------------------")
 # for m in mentors:
 #     print(m)
-
+# print("------------------------------------------------------")
 
 mentees = sorted(parse_mentees("./mentees.csv"))
 N_MENTEES = len(mentees)
 
+# print("------------------------------------------------------")
 # for m in mentees:
-#     print(m.year)
+#     print(m.name)
+# print("------------------------------------------------------")
 
 allocations = allocate_mentors(mentees, mentors)
 N_ALLOCATED = len(allocations)
@@ -228,6 +226,7 @@ with open(SAVE_FILE, "w", encoding="utf8") as f:
         "mentee_year",
         "department",
         "mentor_capacity_left",
+        "mentor_total_capacity",
         "rank",
     ]
     dw = DictWriter(f, fieldnames=f_ns)
@@ -243,17 +242,55 @@ with open(SAVE_FILE, "w", encoding="utf8") as f:
             "mentee_year": me.year,
             "department": me.department,
             "mentor_capacity_left": mr.capacity,
+            "mentor_total_capacity": mr.total_capacity,
             "rank": r,
         }
         dw.writerow(item)
     print(f"Saved the allocations in {SAVE_FILE}.")
 
 me_allocated_ns = [m.name for (m, _, _) in allocations]
-not_allocated = [m for m in mentees if m.name not in me_allocated_ns]
+me_not_allocated = [m for m in mentees if m.name not in me_allocated_ns]
 
-if len(not_allocated) > 0:
-    print("------------------------------------------------------")
-    print("Here are the people who are not allocated any mentors:")
-    for m in not_allocated:
-        print(m)
-        print("------------------------------------------------------")
+with open(SAVE_NOT_MENTEE, "w") as f:
+    field_names = [
+        "mentee_name",
+        "mentee_email",
+        "year",
+    ]
+    dw = DictWriter(f, fieldnames=field_names)
+    dw.writeheader()
+    for m in me_not_allocated:
+        dw.writerow({"mentee_name": m.name, "mentee_email": m.email, "year": m.year})
+
+    print(
+        f"Saved the details of mentees who were not allocated any mentors in {SAVE_NOT_MENTEE}"
+    )
+
+mr_allocated_ns = [m.name for (_, m, _) in allocations]
+mr_not_allocated = [m for m in mentors if m.name not in mr_allocated_ns]
+
+with open(SAVE_NOT_MENTOR, "w") as f:
+    field_names = [
+        "mentor_name",
+        "mentor_email",
+    ]
+    dw = DictWriter(f, fieldnames=field_names)
+    dw.writeheader()
+    for m in mr_not_allocated:
+        dw.writerow(
+            {
+                "mentor_name": m.name,
+                "mentor_email": m.email,
+            }
+        )
+
+    print(
+        f"Saved the details of mentees who were not allocated any mentors in {SAVE_NOT_MENTOR}"
+    )
+
+# if len(not_allocated) > 0:
+#     print("------------------------------------------------------")
+#     print("Here are the people who are not allocated any mentors:")
+#     for m in not_allocated:
+#         print(m)
+#         print("------------------------------------------------------")
